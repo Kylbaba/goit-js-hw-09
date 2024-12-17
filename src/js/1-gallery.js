63a17398d34e784c8c57bd6601ef8ad973ee7b79
Tatiana <petrenkotanya18@gmail.com>
2024-12-17 23:14:15 +0200

    changed form

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,1 +1,8 @@
-console.log("Galery");
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const lightbox = new SimpleLightbox('.gallery a',{
+
+});
+
+console.log(SimpleLightbox);
