eb447cc60f8b01f79792c32aa893b0ce53127cd4
Kylbaba <34373088+Kylbaba@users.noreply.github.com>
2024-12-17 21:18:07 +0000

    Deploying to gh-pages from @ Kylbaba/goit-js-hw-09@07444bf4f2e9bf6f0d9e8a685be63ec27d10ea3c 🚀

--- 1-gallery.js
+++ 1-gallery.js
@@ -1,2 +1,2 @@
-import"./assets/styles-B4s-4mBC.js";console.log("Galery");
+import"./assets/styles-B4s-4mBC.js";import{S as o}from"./assets/vendor-CgTBfC_f.js";new o(".gallery a",{});console.log(o);
 //# sourceMappingURL=1-gallery.js.map
